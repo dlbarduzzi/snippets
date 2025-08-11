8b9a0b200bf5d907711b44f2edf2e6270492e6ac
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-08-11 15:28:11 -0500

    Fix github action env variables

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,4 +4,5 @@ process.env.APP_URL = "http://localhost:3000"
 process.env.APP_PORT = "3000"
 process.env.IS_LOG_STACK_ALLOWED = "false"
 process.env.DATABASE_URL = "postgresql://test_user:test_pass@localhost:5432/test_db"
+process.env.SNIPPETS_SECRET = "testSecret123456789"
 /* eslint-enable node/no-process-env */
