05cd211b9887db7b991512d951a07c55c891a035
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-08-11 08:54:28 -0500

    Fix test env variables

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,4 +2,5 @@
 process.env.LOG_LEVEL = "silent"
 process.env.APP_URL = "http://localhost:3000"
 process.env.APP_PORT = "3000"
+process.env.IS_LOG_STACK_ALLOWED = "false"
 /* eslint-enable node/no-process-env */
